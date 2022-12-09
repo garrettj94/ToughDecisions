2462f57a60d1b04ef376e38cc1dd18697c359025
Brian McKenna <brianmckennaman@gmail.com>
2022-12-08 21:44:58 -0500

    added model for questions

---
+++ server/models/Question.js
@@ -0,0 +1,16 @@
+const { Schema } = require('mongoose');
+
+const questionSchema = new Schema({
+    text: {
+        type: String,
+        required: true,
+    },
+    questionId: {
+        type: String,
+        required: true,
+    }
+})
+
+const Question = model('Question', questionSchema);
+
+module.exports = Question
