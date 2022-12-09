09ebbd04f10bbbf0120848f9e711f115baa98427
Christian Smith <christian.46.smith@gmail.com>
2022-12-08 19:55:26 -0500

    Added User Type, Auth Type, and User Query and Mutations

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,27 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(username: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        updateUser(username: String, email: String, password: String): User
+    }
+`;
+
+module.exports = typeDefs;
