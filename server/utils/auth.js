65d31344d7da8816e6ba3a4c0f1fbdb3faa27dfb
Brian McKenna <brianmckennaman@gmail.com>
2022-12-08 19:24:45 -0500

    added models and worked on connections auth and schemas files

---
+++ server/utils/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const secret = ''
+const expiration = '30d';
+
+module.exports = {
+    authMiddleware: function ( { req }) {
+        let token = req.body.token || req.query.token || req.header.authorization;
+
+        if (req.headers.authorization) {
+            token = token.split(' ').pop().trim();
+        }
+
+        if (!token) {
+            return req;
+        }
+
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log(err);
+        }
+        return req;
+    },
+    signToken: function ({ username, email, _id }) {
+        const payload = { username, email, _id };
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+};
