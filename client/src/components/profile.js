2a81fb42ce5d79c4d98f2d9618b4f57ded25074f
BrandonN64 <Brandon.nastri@gmail.com>
2022-12-12 21:27:43 -0500

    Updated the profile, added react-router to navbar

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { Card, CardGroup, Col, Row } from "react-bootstrap";
 function profile() {
     return (
         <div>
-
+            <h1>User's Profile</h1>
+            <img className="thumbnail" src="https://via.placeholder.com/150"></img>
+            <br></br>
+            <br></br>
+            <Row md={3}>
+                    <Card >
+                        <Card.Body>Step on a bed of hot charcoal</Card.Body>
+                    </Card>
+                    <Card >
+                        <Card.Body>Step on a bed of hot charcoal</Card.Body>
+                    </Card>
+                    <Card >
+                        <Card.Body>Step on a bed of hot charcoal</Card.Body>
+                    </Card>
+                    <Card >
+                        <Card.Body>Step on a bed of hot charcoal</Card.Body>
+                    </Card>
+                    <Card >
+                        <Card.Body>Step on a bed of hot charcoal</Card.Body>
+                    </Card>
+            </Row>
         </div>
     );
 }
