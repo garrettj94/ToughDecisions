db6db06a3d2b0f53e6ecd47bb9b24a35fa960204
BrandonN64 <Brandon.nastri@gmail.com>
2022-12-15 17:59:35 -0500

    added a stripe payment link to the store page, along with an icon and a price

--- client/src/components/store.js
+++ client/src/components/store.js
@@ -2,56 +2,17 @@ import React from "react";
 import { Card, Row, Button } from "react-bootstrap"
 function store() {
     return (
-        <div>
+        <div id="main">
             <Row md={2}>
                 <Card style={{ width: '20rem' }}>
                     <Card.Body>
-                        <Card.Img variant="top" src="https://via.placeholder.com/180x100" />
-                        <Card.Title>Example Cosmetic</Card.Title>
+                        <Card.Img variant="top" src="https://tinyurl.com/2hcrznzj" />
+                        <Card.Title>1000 Doubloons</Card.Title>
                         <Card.Text>
-                            Example Description of Cosmetic
+                            A mediocre pile of doubloons. Should be enough to get you some cool cosmetics!
                         </Card.Text>
-                        <Button variant="primary">Go to checkout</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '20rem' }}>
-                    <Card.Body>
-                        <Card.Img variant="top" src="https://via.placeholder.com/180x100" />
-                        <Card.Title>Example Cosmetic</Card.Title>
-                        <Card.Text>
-                            Example Description of Cosmetic
-                        </Card.Text>
-                        <Button variant="primary">Go to checkout</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '20rem' }}>
-                    <Card.Body>
-                        <Card.Img variant="top" src="https://via.placeholder.com/180x100" />
-                        <Card.Title>Example Cosmetic</Card.Title>
-                        <Card.Text>
-                            Example Description of Cosmetic
-                        </Card.Text>
-                        <Button variant="primary">Go to checkout</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '20rem' }}>
-                    <Card.Body>
-                        <Card.Img variant="top" src="https://via.placeholder.com/180x100" />
-                        <Card.Title>Example Cosmetic</Card.Title>
-                        <Card.Text>
-                            Example Description of Cosmetic
-                        </Card.Text>
-                        <Button variant="primary">Go to checkout</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '20rem' }}>
-                    <Card.Body>
-                        <Card.Img variant="top" src="https://via.placeholder.com/180x100" />
-                        <Card.Title>Example Cosmetic</Card.Title>
-                        <Card.Text>
-                            Example Description of Cosmetic
-                        </Card.Text>
-                        <Button variant="primary">Go to checkout</Button>
+                        <Button variant="primary" href="#">$5.00</Button>
+                        {/* <Button variant="primary" href="https://buy.stripe.com/6oEbIQbKhewv9FKcMM">$5.00</Button> */}
                     </Card.Body>
                 </Card>
             </Row>
